83983ff66f2b261936ae4faa374c8a4bfd40c95c
engageintellect <jc9361@gmail.com>
2024-02-28 02:52:18 -0800

    adding client

--- client/src/lib/components/Roadmap.svelte
+++ client/src/lib/components/Roadmap.svelte
@@ -1,18 +1,11 @@
-<ul class="timeline timeline-vertical">
+<ul class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
 	<li>
-		<div class="timeline-start timeline-box">
-			<div class="text-xl font-bold">hello</div>
-			Amet ullamco pariatur exercitation ad id duis commodo esse cupidatat aliquip. Non sunt ipsum velit
-			excepteur culpa irure. Officia proident labore pariatur culpa. Sit minim sunt fugiat labore consectetur
-			cillum ea nulla deserunt amet sit pariatur consequat esse velit. Culpa veniam deserunt aute ullamco
-			duis proident esse commodo nisi culpa.
-		</div>
 		<div class="timeline-middle">
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				viewBox="0 0 20 20"
 				fill="currentColor"
-				class="text-primary h-5 w-5"
+				class="h-5 w-5"
 				><path
 					fill-rule="evenodd"
 					d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
@@ -20,49 +13,13 @@
 				/></svg
 			>
 		</div>
-		<hr class="bg-primary" />
-	</li>
-	<li>
-		<hr class="bg-primary" />
-		<div class="timeline-middle">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 20 20"
-				fill="currentColor"
-				class="text-primary h-5 w-5"
-				><path
-					fill-rule="evenodd"
-					d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-					clip-rule="evenodd"
-				/></svg
-			>
-		</div>
-		<div class="timeline-end timeline-box">
-			<div class="text-xl font-bold">hello</div>
-			Minim Magna fugiat officia elit ullamco esse. eu elit ut cupidatat Lorem elit.
-		</div>
-		<hr class="bg-primary" />
-	</li>
-	<li>
-		<hr class="bg-primary" />
-		<div class="timeline-start timeline-box">
-			<div class="text-xl font-bold">hello</div>
-			Nulla id nulla sint et. Exercitation laboris et officia exercitation amet in nostrud. Eiusmod anim
-			aliquip ex ut anim irure ullamco. Sit eiusmod sint aute voluptate consectetur magna ipsum duis
-			tempor duis incididunt nulla veniam.
-		</div>
-		<div class="timeline-middle">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 20 20"
-				fill="currentColor"
-				class="text-primary h-5 w-5"
-				><path
-					fill-rule="evenodd"
-					d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-					clip-rule="evenodd"
-				/></svg
-			>
+		<div class="timeline-start mb-10 md:text-end">
+			<time class="font-mono italic">1984</time>
+			<div class="text-lg font-black">First Macintosh computer</div>
+			The Apple Macintosh—later rebranded as the Macintosh 128K—is the original Apple Macintosh personal
+			computer. It played a pivotal role in establishing desktop publishing as a general office function.
+			The motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were housed in a beige case with
+			integrated carrying handle; it came with a keyboard and single-button mouse.
 		</div>
 		<hr />
 	</li>
@@ -81,21 +38,17 @@
 				/></svg
 			>
 		</div>
-		<div class="timeline-end timeline-box">
-			<div class="text-xl font-bold">hello</div>
-			Enim nulla non eiusmod consequat excepteur dolore. Laborum eu ea ea qui laborum deserunt. Enim
-			ex qui labore minim aliquip dolor incididunt laborum consectetur esse nulla duis in. Minim
+		<div class="timeline-end mb-10">
+			<time class="font-mono italic">1998</time>
+			<div class="text-lg font-black">iMac</div>
+			iMac is a family of all-in-one Mac desktop computers designed and built by Apple Inc. It has been
+			the primary part of Apple's consumer desktop offerings since its debut in August 1998, and has
+			evolved through seven distinct forms
 		</div>
 		<hr />
 	</li>
 	<li>
 		<hr />
-		<div class="timeline-start timeline-box">
-			<div class="text-xl font-bold">hello</div>
-			Laborum aliquip aliqua eu labore eiusmod Lorem velit sit nisi adipisicing sint. Non qui sit ad
-			voluptate aliquip aliquip. Consequat tempor sunt duis ipsum aliquip voluptate voluptate aliqua
-			est veniam dolore.
-		</div>
 		<div class="timeline-middle">
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
@@ -108,6 +61,65 @@
 					clip-rule="evenodd"
 				/></svg
 			>
+		</div>
+		<div class="timeline-start mb-10 md:text-end">
+			<time class="font-mono italic">2001</time>
+			<div class="text-lg font-black">iPod</div>
+			The iPod is a discontinued series of portable media players and multi-purpose mobile devices designed
+			and marketed by Apple Inc. The first version was released on October 23, 2001, about 8+1⁄2 months
+			after the Macintosh version of iTunes was released. Apple sold an estimated 450 million iPod products
+			as of 2022. Apple discontinued the iPod product line on May 10, 2022. At over 20 years, the iPod
+			brand is the oldest to be discontinued by Apple
+		</div>
+		<hr />
+	</li>
+	<li>
+		<hr />
+		<div class="timeline-middle">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 20 20"
+				fill="currentColor"
+				class="h-5 w-5"
+				><path
+					fill-rule="evenodd"
+					d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+					clip-rule="evenodd"
+				/></svg
+			>
+		</div>
+		<div class="timeline-end mb-10">
+			<time class="font-mono italic">2007</time>
+			<div class="text-lg font-black">iPhone</div>
+			iPhone is a line of smartphones produced by Apple Inc. that use Apple's own iOS mobile operating
+			system. The first-generation iPhone was announced by then-Apple CEO Steve Jobs on January 9, 2007.
+			Since then, Apple has annually released new iPhone models and iOS updates. As of November 1, 2018,
+			more than 2.2 billion iPhones had been sold. As of 2022, the iPhone accounts for 15.6% of global
+			smartphone market share
+		</div>
+		<hr />
+	</li>
+	<li>
+		<hr />
+		<div class="timeline-middle">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 20 20"
+				fill="currentColor"
+				class="h-5 w-5"
+				><path
+					fill-rule="evenodd"
+					d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+					clip-rule="evenodd"
+				/></svg
+			>
+		</div>
+		<div class="timeline-start mb-10 md:text-end">
+			<time class="font-mono italic">2015</time>
+			<div class="text-lg font-black">Apple Watch</div>
+			The Apple Watch is a line of smartwatches produced by Apple Inc. It incorporates fitness tracking,
+			health-oriented capabilities, and wireless telecommunication, and integrates with iOS and other
+			Apple products and services
 		</div>
 	</li>
 </ul>
